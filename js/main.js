a80d18be344676933d14264e656ec8800a850eaa
galih w <72421097+galihw@users.noreply.github.com>
2026-08-28 08:53:19 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,23 +3,35 @@ document.querySelectorAll('.btnDetail').forEach(item => {
         let parent = e.target.parentNode.parentNode;
 
         let gambar = parent.querySelector('.card-img-top').src;
+        let color = parent.querySelector('.card-img-top').style.backgroundColor;
+		
+		// mengambil nomor di string gambar
+		let hasil1 = gambar.charAt(gambar.length-7);
+		let hasil2 = gambar[gambar.length-6];
+		let hasil3 = gambar[gambar.length-5];
+		let no = 1*(hasil1+''+hasil2+''+hasil3);
+		if(hasil1=='s') no = 1*(hasil2+''+hasil3);
+		else if(hasil2=='s') no = 1*(hasil3);
+		//console.log(hasil1,hasil2,no);
+		//console.log(parent.id)
+		gambar = 'images/'+(noklik+1)+'/kaos'+no+'.png';
+		let gambar2 = 'images/'+(noklik+1)+'/orang'+no+'.jpg';
+		//console.log(hasil1,hasil2,hasil3);
+		
+		
+		
+		
         let harga = parent.querySelector('.harga').innerHTML;
         let judul = parent.querySelector('.card-text').innerHTML;
         let deskripsi = parent.querySelector('.deskripsi') ? parent.querySelector('.deskripsi').innerHTML : '<i>tidak ada informasi yang tersedia</i>';
 
-		// mengambil nomor di string gambar
-		let hasil1 = gambar.charAt(gambar.length-6);
-		let hasil2 = gambar[gambar.length-5];
-		let no = 1*(hasil1+''+hasil2);
-		if(hasil1=='s') no = 1*(hasil2);
-		console.log(hasil1,hasil2,no);
-		let gambar2 = 'images/orang'+no+'.jpg';
 		
         let tombolModal = document.querySelector('.btnModal');
         tombolModal.click();
 
         document.querySelector('.modalTitle').innerHTML = judul;
         let image = document.createElement('img');
+		image.style.backgroundColor = color;
         image.src = gambar;
         image.classList.add('w-100');
         document.querySelector('.modalImage').innerHTML = '';
@@ -32,7 +44,7 @@ document.querySelectorAll('.btnDetail').forEach(item => {
         document.querySelector('.modalHarga').innerHTML = harga;
 
         const nohp = '6285714408830';
-        let pesan = `https://api.whatsapp.com/send?phone=${nohp}&text=Halo Bang, saya mau pesan produk ini ${gambar}`;
+        let pesan = `https://api.whatsapp.com/send?phone=${nohp}&text=Halo Bang, saya mau pesan produk ini ${judul}`;
 
         document.querySelector('.btnBeli').href = pesan;
     });
